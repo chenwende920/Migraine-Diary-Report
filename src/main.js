3365ef4a9faee9b0e5f71b627a11ce376c27a4a2
chenwende920 <4a62c054@stust.edu.tw>
2023-04-22 19:44:11 +0800

    create new project

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './assets/tailwind.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).use(ElementPlus).mount('#app')
